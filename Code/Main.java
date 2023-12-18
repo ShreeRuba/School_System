32add50bc4c814ac865b7c130f791add33b95c80
ShreeRuba <rubashree0361@gmail.com>
2023-12-18 12:36:53 +0530

    Files Added

--- Code/Main.java
+++ Code/Main.java
@@ -47,30 +47,30 @@ public class Main {
         // Example usage
 
         // Create students
-        Student student1 = new Student("Klaus", "Mikaelson", 1, 85, 1);
-        Student student2 = new Student("Stefan", "Salvatore", 2, 66, 1);
-        Student student3 = new Student("Caroline", "Forbes", 3, 55, 1);
-        Student student4 = new Student("Hayley", "Marshall", 4, 70, 1);
-        Student student5 = new Student("Atlas", "Corrigan", 5, 90, 1);
-        Student student6 = new Student("Tom", "Hiddleston", 6, 98, 1);
+        Student stud1 = new Student("Klaus", "Mikaelson", 1, 85, 1);
+        Student stud2 = new Student("Stefan", "Salvatore", 2, 66, 1);
+        Student stud3 = new Student("Caroline", "Forbes", 3, 55, 1);
+        Student stud4 = new Student("Hayley", "Marshall", 4, 70, 1);
+        Student stud5 = new Student("Atlas", "Corrigan", 5, 90, 1);
+        Student stud6 = new Student("Tom", "Hiddleston", 6, 98, 1);
 
 
         // Create course
-        Course course = new Course("Java Programming", "Prof. Johnson", 2023);
+        Course course = new Course("Junior FullStack Developer (JSD)", "Prof. Annamalai and Prof. Chetan", 2023);
 
         // Enroll individual students
-        course.enroll(student1);
-        course.enroll(student2);
-        course.enroll(student4);
-        course.enroll(student5);
-        course.enroll(student6);
+        course.enroll(stud1);
+        course.enroll(stud2);
+        course.enroll(stud4);
+        course.enroll(stud5);
+        course.enroll(stud6);
 
         // Print student count and best grade
         System.out.println("Number of students: " + course.countStudents());
         System.out.println("Best grade in the course: " + course.bestGrade());
 
         // Enroll a list of students
-        List<Student> studentList = Arrays.asList(student2, student3, student4);
+        List<Student> studentList = Arrays.asList(stud2, stud3, stud4);
         course.enroll(studentList);
 
         // Print updated student count and best grade
